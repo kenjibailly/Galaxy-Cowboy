0840c6907964a0e621a38f780a0e76d6e3d1b4be
Kenji Bailly <kenjibailly@gmail.com>
2020-06-23 19:27:00 +0200

    help page

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ const helpEmbed = new Discord.RichEmbed()
 	.setThumbnail("attachment://osalien.jpg")
 	.addBlankField()
 	.addField("Create Weekly Poll", `\`${config.prefix}weekly "Title" "Starting Date" "Ending Date"\``+"\nFormat: YYYY-MM-DD format\ndates are optional fields, default starts today, ends in 7 days")
-	.addField("Create Weekly Custom Poll", `\`${config.prefix} After weekly "Title" "start date" "end date" you can put "Custom Description"\``)
+	.addField("Create Weekly Custom Poll", `\`After weekly "Title"${config.prefix}weekly "start date" "end date" you can put "Custom Description"\``)
 	.addField("Create Y/N poll", `\`${config.prefix}poll "Question"\``)
 	.addField("Create complex poll [2-10 answers]", `\`${config.prefix}poll "Question" "Option 1" "Option 2" ["Option 3" ...]\` (quotes are necessary)`)
 	.addField("Timed polls that close automatically", `\`${config.prefix}{weekly/poll} time=X{s|m|h|d} ...\`\nX = length + secons, minutes, hours, days. 
 	This can be added in all polls before "Poll Question / Weekly Title"`)
-	.addField("See results of a poll and close the voting", `\`${config.prefix} end ID\`\nwhere ID is the poll id wich
+	.addField("See results of a poll and close the voting", `\`${config.prefix}end ID\`\nwhere ID is the poll id wich
 		appears at the end of the poll`)
-	.addField("See examples", `\`${config.prefix} examples\``)
+	.addField("See examples", `\`${config.prefix}examples\``)
 	.addBlankField()
 	.addField("About", "The bot has been created by Zep, leader and founder of Galaxy Cowboys. Feel free to report bugs.")
 	.setFooter("Credits to Zheoni for being able to use the source code of [here](http://github.com/Zheoni/VotaBot) and make this happen", 'attachment://zep.jpg')
