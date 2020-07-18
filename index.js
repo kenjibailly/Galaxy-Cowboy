1f678d4afb8841ccd8cb3214b37bce521e41f6dd
Kenji Bailly <kenjibailly@gmail.com>
2020-07-19 01:33:18 +0200

    donate

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var inputid;
 var w;
 const client = new Discord.Client();
 const prefix = String("`"+config.prefix+"`");
-const commandSyntaxRegex = /(help)|(poll\s(time=\d+([smhd]?\s))?("[^"\n]+"\s?){1,11})|(weekly\s(time=\d+([smhd]?\s))?("[^"\n]+"\s?){1,11})|(update\s\d+)|(examples)|(end\s\d+)|(invite)$/;
+const commandSyntaxRegex = /(help)|(poll\s(time=\d+([smhd]?\s))?("[^"\n]+"\s?){1,11})|(weekly\s(time=\d+([smhd]?\s))?("[^"\n]+"\s?){1,11})|(update\s\d+)|(examples)|(end\s\d+)|(invite)|(donate)$/;
 const prefixSyntaxRegex = new RegExp(`^[${config.prefix}]`);
 const helpEmbed = new Discord.RichEmbed()
 	.setTitle("❓ ┊ Galaxy Cowboys's Commands")
@@ -27,6 +27,8 @@ const helpEmbed = new Discord.RichEmbed()
 	.addField("<:gcstar:730505901529759784>	See results of a poll and close the voting", `\`${config.prefix}end ID\`\nwhere ID is the poll id which appears at the end of the poll`)
 	.addField("<:gcstar:730505901529759784>	See examples", `\`${config.prefix}examples\``)
 	.addField("<:gcstar:730505901529759784>	Call to action Weekly Poll", `\`${config.prefix}update ID\`\nwhere ID is the poll id which appears at the end of the poll`)
+	.addBlankField()
+	.addField("<:paypal:734189737077637121>	Donate", `\`${config.prefix}donate\`\nDonate to help keeping the uptime of the bot`)
 	.addBlankField()
 	.addField("About", "The bot has been created by Zep, leader and founder of Galaxy Cowboys. Feel free to report bugs.")
 	.setFooter("Credits to Zheoni for being able to use the source code of Votabot and make this happen", 'attachment://zep.jpg')
@@ -49,6 +51,14 @@ const examplesEmbed = new Discord.RichEmbed()
 	.attachFiles(['./assets/zep.jpg'])
 	.setFooter("The bot has been created by Zep, leader and founder of Galaxy Cowboys.\nFeel free to report bugs.", 'attachment://zep.jpg')
 	.setColor("#DDA0DD");
+let donateEmbed = new Discord.RichEmbed()
+		.attachFiles(['./assets/donateQR.png', './assets/zep.jpg'])
+		.setTitle(`<:paypal:734189737077637121>┊ Thank you for donating!`)
+		.addField(`Donations can be made using PayPal`, "[PayPal Link](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=QCX6LUQA9CZFC&source=url)")
+		.setImage('attachment://donateQR.png')
+		.setDescription(`Thank you for helping with the uptime of the bot.`)
+		.setColor("#d596ff")
+		.setFooter("Thank you so much! :)", 'attachment://zep.jpg');
 const weeklyEmbed = new Discord.RichEmbed()
 	.setAuthor("Galaxy Zep")
 	.setDescription("<:Monday:723595665325948929>"+"Monday")
@@ -335,7 +345,7 @@ client.on("message", async (msg) => {
 	if(msg.content.startsWith(config.prefix) && msg.content !== config.prefix){
 		const command = msg.content.slice(config.prefix.length);
 		let args = parseToArgs(msg);
-		var words = ["help", "poll ", "weekly", "examples", "update", "end", "invite"];
+		var words = ["help", "poll ", "weekly", "examples", "update", "end", "invite", "donate"];
 		if(words.includes(args[0])) {
 		if (commandSyntaxRegex.test(command)) {
 			
@@ -350,6 +360,9 @@ client.on("message", async (msg) => {
 					case "examples":
 						dmChannel = await msg.author.createDM();
 						dmChannel.send({ embed: examplesEmbed });
+						break;
+					case "donate":
+						msg.reply({ embed: donateEmbed});
 						break;
 					case "weekly":
 						if (!isDM) {
