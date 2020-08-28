7850cbeda38a83bbcde8ad2ec5632d725ba17cb7
Kenji Bailly <kenjibailly@gmail.com>
2020-08-28 23:02:49 +0200

    update

--- functions/richPresence.js
+++ functions/richPresence.js
@@ -9,13 +9,13 @@ const activity = (client) => {
 	setTimeout(() => {
 		var discordActivityStatus;
 		var discordActivityType;
-		if (delay > 3) {
+		if (delay > 2) {
 			delay = 0;
 		}
 		switch (delay) {
 			case 0:
-				discordActivityStatus = "Hey, test me out!";
-				discordActivityType = "LISTENING";
+				discordActivityStatus = "kb-galaxy-cowboy.ga";
+				discordActivityType = "WATCHING";
 				break;	
 			case 1:
 				discordActivityStatus = `${config.prefix}help`;
@@ -25,10 +25,10 @@ const activity = (client) => {
 				discordActivityStatus = `${config.prefix}examples`;
 				discordActivityType = "WATCHING";
 				break;
-			case 3:
-				discordActivityStatus = "Guild Wars 2";
-				discordActivityType = "PLAYING";
-				break;
+			// case 3:
+			// 	discordActivityStatus = "http://kb-galaxy-cowboy.ga";
+			// 	discordActivityType = "WATCHING";
+			// 	break;
 			default:
 
 				break;
